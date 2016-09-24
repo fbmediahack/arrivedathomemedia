5ad299df1685dbaea6319e711e427adb910689d0
anikiki <anikiki@gmail.com>
2016-09-24 14:30:54 +0100

    Added notification receiver.

---
+++ ArriveatHomemedia/app/src/main/java/com/example/arriveathomemedia/Mood.java
@@ -0,0 +1,10 @@
+package com.example.arriveathomemedia;
+
+/**
+ * Created by ana on 24/09/16.
+ */
+public final class Mood {
+    public static final String COOL = "Cool";
+    public static final String POOP = "Poop";
+    public static final String BOOM = "Boom";
+}
